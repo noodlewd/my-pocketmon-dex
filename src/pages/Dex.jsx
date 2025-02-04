d5e1410451519cd46b716f5e54dc8543b0718aeb
noodlewd <michel35@naver.com>
2025-02-04 20:21:50 +0900

    fix : prev 사용한 삭제 로직 수정

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
+import monsterBallImg from "../assets/poketball.jpg";
 
 const Dex = () => {
   const [myPokeBall, setMyPokeBall] = useState([]);
@@ -16,8 +17,14 @@ const Dex = () => {
 
   // 대쉬보드 포켓몬 삭제 로직
   const removePokemon = (pokemonId) => {
-    const updateList = myPokeBall.filter((item) => item.id !== pokemonId);
-    setMyPokeBall(updateList);
+    setMyPokeBall((prev) => {
+      const index = prev.findIndex((pokemon) => pokemon.id === pokemonId);
+      if (index === -1) return prev; // 만약 포켓몬이 없다면 기존 배열 유지
+
+      const updatedList = [...prev]; // 기존 배열 복사
+      updatedList.splice(index, 1); // 첫 번째로 찾은 포켓몬만 삭제
+      return updatedList; // 업데이트된 배열 반환
+    });
   };
 
   return (
